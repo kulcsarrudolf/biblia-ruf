2454299368d08065bcf724cc7d288835092986df
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2022-07-05 17:51:09 +0300

    Removed axios

--- src/book-details.js
+++ src/book-details.js
@@ -1,19 +1,8 @@
-const { default: axios } = require("axios");
 const { readJSONFile } = require("./utils");
-
-const fetchBookDetails = async () => {
-  const BOOK_DETAILS_URL =
-    "https://gist.githubusercontent.com/kulcsarrudolf/798cebd4ca163a925b272debad7d5a3b/raw/cd979d3cd801d730ccafa53c4b7fe3c032c5a025/biblia.json";
-
-  const response = await axios.get(BOOK_DETAILS_URL);
-
-  return response.data.book;
-};
 
 const getBookDetails = async (book) => {
   const verses = new Map();
-
-  const books = await fetchBookDetails();
+  const books = await readJSONFile("json/biblia.json");
   const res = books.find((b) => b.toc3 === book);
   const bookContent = await readJSONFile(`json/${book}.json`);
 
