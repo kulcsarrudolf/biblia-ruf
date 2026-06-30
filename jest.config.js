dabf1e9f6f20631c92437a1d58fe66dd8720e0b3
Kulcsár Rudolf <kulcsarrudolf@gmail.com>
2026-06-30 10:55:32 +0200

    test: add jest coverage reporting (#5)
    Add a coverage script and collectCoverageFrom config (text + lcov).
    
    Ignore the coverage output directory.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  roots: ["<rootDir>/tests"],
-  testMatch: ["**/*.test.ts"],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts', '!src/cli/**'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
 };
